76af36b5d456e0ebd09c69f202b7fa81a0e92de5
Aiden <myjingu@naver.com>
2022-07-10 13:07:43 +0900

    commit

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/* App.js에 있는 function App() 부분을 불러온다 */}
     <App />
   </React.StrictMode>
 );
